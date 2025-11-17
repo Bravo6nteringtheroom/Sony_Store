01f633ab1a6aa8a65ea90eea412f6b823065f009
Bravo6nteringtheroom <mohamadOwidate2@gmail.com>
2025-11-17 22:02:09 +0200

    hey guys just added the Login Page and the Signup page give me your opinion on the Login Page made purely without ai and your free to customize the Signup page

---
+++ SignUpPage.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Css/SignUpPage.style.css">
+</head>
+<body>
+    <form action="SignUpPage.php" method="POST" id="SignUpForm">
+        <h2 id="Sign_title">Sign Up</h2>
+        <div id="F_L">
+            <input type="text" name="First_name" placeholder="First_name">
+            <input type="text" name="Last_name" placeholder="Last_name">
+        </div>
+        <input type="email" name="new_Email" placeholder="Email">
+        <input type="password" name="new_Password" placeholder="Password">
+        <input type="password" name="confirm_Password" placeholder="Confirm Password">
+        <button id="Sign_Up_btn">Sign Up</button>
+    </form>
+
+    <?php
+        if (isset($_POST["First_name"], $_POST["Last_name"], $_POST["new_Email"], $_POST["new_Password"], $_POST["confirm_Password"]) &&!empty($_POST["First_name"]) &&!empty($_POST["Last_name"]) &&!empty($_POST["new_Email"]) &&!empty($_POST["new_Password"]) &&!empty($_POST["confirm_Password"])) {
+            $First_name = $_POST["First_name"];
+            $Last_name = $_POST["Last_name"];
+            $Email = $_POST["new_Email"];
+            $Password = $_POST["new_Password"];
+            $Password_Confirm = $_POST["confirm_Password"];
+
+            if($Password === $Password_Confirm){
+            echo"These 2 strings are equal";
+        }
+        if(str_contains($Email,'@gmail.com')){
+            //All good here guys
+        }else{
+            //Make that bitch enter the required email
+        }
+        if(strlen($Password) > 8 && preg_match('/\\d/',$Password)){
+            //echo"The password is greater than 8" and also has some digits in it;
+        }else{
+            //echo"The password is less tha 8";
+        }
+    } else {
+            echo"Please fill in the following stuff:";
+        }
+    ?>
+</body>
+</html>
